978597cb206381958b4750cf9216d2f2a21b1d82
Eugene Westbrook <eugenewestbrook@gmail.com>
2014-11-02 17:49:57 -0500

    Styling and functionality for modal

--- app/scripts/controllers/picture_modal_controller.js
+++ app/scripts/controllers/picture_modal_controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('angularWeddingApp')
-  .controller('PictureModalCtrl', function($scope, $http, thumbnail, full){
+  .controller('PictureModalCtrl', function($scope, $http, $modalInstance, 
+      thumbnail, full){
 
     var fileName = function(full_file){
       return full_file.substring(full_file.lastIndexOf('/') + 1);
@@ -12,4 +13,8 @@ angular.module('angularWeddingApp')
 
     $scope.full = full;
     $scope.fullName = fileName(full);
+
+    $scope.close = function() {
+      $modalInstance.close();
+    }
 });
