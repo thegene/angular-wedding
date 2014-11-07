f15abedd5d701c1f864f546197e218212da240bf
Eugene Westbrook <eugenewestbrook@gmail.com>
2014-11-06 21:59:25 -0500

    Adds simple end to end tests for picturelist and modals

--- test/e2e/pictureList.e2e.js
+++ test/e2e/pictureList.e2e.js
@@ -1,5 +1,36 @@
-describe('blah', function(){
-  it('is', function(){
-    expect(false).toBe(true);
+'use strict';
+
+describe('Main', function(){
+  browser.get('http://localhost:9000/')
+  var getPictureList = function() {
+    return element.all(by.repeater('picture in pictures'));
+  };
+
+  describe('Picture List', function(){
+    it('Has pictures', function(){
+      expect(getPictureList().count()).toEqual(3);
+    });
   });
+
+  describe('Picture modal', function(){
+
+    var clickOnPicture = function(){
+      getPictureList().get(0).click();
+    }
+
+    it('displays a modal when clicked', function(){
+      expect($('.picture-modal').isPresent()).toBe(false);
+      clickOnPicture();
+      expect($('.picture-modal').isPresent()).toBe(true);
+      $('.picture-modal .close-picture').click();
+      expect($('.picture-modal').isPresent()).toBe(false);
+    });
+
+    it('displays two download links when clicked', function(){
+      clickOnPicture();
+      expect(element.all(by.css('.download a')).count()).toEqual(2);
+    });
+
+  });
+
 });
